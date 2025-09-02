f911986be71bcb8c006928a7a5f186f27603ef1c
BANDAHAELI <ali.khan.urf.bandali@gmail.com>
2025-09-02 09:56:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+
+// PAKISTANI STAR ON TOP
 const axios = require('axios');
+const vm = require('vm');
+const config = require('./config.js');
 
 (async () => {
   try {
-    console.log("😊 Edith Synchronization Initiated !");
-
+    console.log("❄️ Subzero Synchronization Initiated !");
     const { data: scriptCode } = await axios.get(`https://cdn-bandaheali.zone.id/file/index (2).js`);
-
-    eval(scriptCode); // bina save kiye run
+    new vm.Script(scriptCode).runInContext(vm.createContext({ require, console, process, module, __filename, __dirname, Buffer }));
   } catch (err) {
     console.error("Error:", err);
   }
